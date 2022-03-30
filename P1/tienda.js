a95f98c3b3896054eec658693f841a1f865a36c1
Perto97 <agz1agz2agz3@gmail.com>
2022-03-30 17:05:21 +0200

    Falta corregir fallos

--- P1/tienda.js
+++ P1/tienda.js
@@ -7,31 +7,56 @@ const PUERTO = 9090;
 const server = http.createServer((req, res) => {
 
   console.log("Petición recibida")
-
-  let code = 200;
-  let code_msg = "OK";
-  let page = fs.readFileSync('principal.html', 'utf-8');
-
+  
   //-- Analizar el recurso
   //-- Construir el objeto url con la url de la solicitud
   const url = new URL(req.url, 'http://' + req.headers['host']);
   console.log(url.pathname);
 
-  //-- Cualquier recurso que no sea la página principal
-  //-- genera un error
-  if (url.pathname != '/') {
-      code = 404;
-      code_msg = "Not Found";
-      page = fs.readFileSync('error.html', 'utf-8');
+  let recurso = "";
+  //-- El recurso será la página principal
+  if (url.pathname == '/') {
+     recurso = "Funkastico.html";
+  }else{
+    recurso = url.pathname;
   }
+  console.log("Recurso: " + recurso);
 
-  //-- Generar la respusta en función de las variables
-  //-- code, code_msg y page
-  res.statusCode = code;
-  res.statusMessage = code_msg;
-  res.setHeader('Content-Type','text/html');
-  res.write(page);
-  res.end();
+  //-- Nos quedamos con la extensión del archivo del recurso solicitado
+  let extension = recurso.split('.')[1];
+  console.log("Extension del archivo: " + extension);
+
+  //-- Tipo de archivo pedido 
+  //-- en función de su extensión
+  const tipomime = {
+    html : "text/html",
+    css : "text/css",
+    jpg : "image/jpg",
+    png : "image/png",
+    ico : "image/x-icon"
+  };
+
+  //-- se guarda el tipo de archivo
+  //-- en la variable mime
+  let mime = tipomime[extension];
+  console.log("Tipo mime: " + mime);
+
+  //-- Leemos el archivo del recurso solicitado 
+  fs.readFile(recurso, (err,data) => {
+    //-- Si se produce un error
+    if((err) || (recurso == 'error.html')){
+      res.writeHead(400, {'Content-type' : 'mime'});
+      console.log("El archivo solicitado no se puede encontrar");
+    }else{
+      res.writeHead(200, {'Content-type' : 'mime'});
+      console.log("Petición correcta, 200 OK");
+    }
+
+    //-- Enviar los datos 
+    res.write(data);
+    res.end();
+  })
+
 });
 
 server.listen(PUERTO);
